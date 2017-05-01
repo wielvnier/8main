99ecb5967482ecd23fd644519b8443dc457c73e4
wielvnier <filipwielanier@gmail.com>
2017-05-01 13:09:14 +0200

    warunki

---
+++ JS/scripts.js
@@ -0,0 +1,20 @@
+var a = 2
+var b = 3
+
+var value = (a * a) + (2 * a * b) - (b * b)
+
+console.log(‘value’);
+
+if (value > 0 ) {
+
+    console.log(‘Wynik jest dodatni’);
+
+} else if (value < 0 ) {
+
+    console.log(‘Wynik jest ujemny’);
+
+} else {
+
+    console.log(‘Wynik jest równy zero’);
+
+}
