bc4e6d50da0f549643fe585373f2abc219be4aa2
Filip Wielanier <filipwielanier@gmail.com>
2017-05-02 15:50:30 +0200

    dodanie_pluginu_sass

--- gruntfile.js
+++ gruntfile.js
@@ -1,13 +1,25 @@
 module.exports = function(grunt) {
 
+  require('load-grunt-tasks')(grunt); 
+  
   grunt.initConfig({
     jshint: {
       all: ['js/*.js']
+    }
+    options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/main.css': 'sass/main.sass'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['sass']);
 
 };
